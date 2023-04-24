c595eb00e55e48569d64be73034cabc046575c8a
Tulgacan Güdü <tulgacan.gudu@gmail.com>
2023-04-24 20:04:41 +0300

    Add reading using buffer
    Signed-off-by: Tulgacan Güdü <tulgacan.gudu@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	fileNames := os.Args[1:]
 
-	for _, name := range fileNames {
-		f, _ := os.ReadFile(name)
-		fmt.Println(string(f))
+	readBuffer(fileNames, 1024)
+}
+
+func readBuffer(fileNames []string, bufferSize int) error {
+
+	for _, fileName := range fileNames {
+		file, err := os.Open(fileName)
+		if err != nil {
+			return fmt.Errorf("file could not be opened: %w", err)
+		}
+		defer file.Close()
+
+		buffer := make([]byte, bufferSize)
+
+		for {
+			bytesRead, err := file.Read(buffer)
+
+			if err != nil {
+				if err != io.EOF {
+					return fmt.Errorf("error occured while reading file %s: %w", fileName, err)
+				}
+
+				break
+			}
+
+			fmt.Print(string(buffer[:bytesRead]))
+		}
 	}
+
+	return nil
 }
